94b9866dd5846a8f33fd9ac9e913cd46b46847f4
Folyd <lyshuhow@gmail.com>
2020-05-11 23:42:02 +0800

    Give beforeNavigate a default function

--- omnibox.js
+++ omnibox.js
@@ -80,9 +80,9 @@ Omnibox.prototype.bootstrap = function({onSearch, onFormat, onAppend, beforeNavi
 
     chrome.omnibox.onInputEntered.addListener((content, disposition) => {
         let result;
-        if (beforeNavigate) {
-            content = beforeNavigate(content);
-        }
+        // Give beforeNavigate a default function
+        beforeNavigate = beforeNavigate || ((s) => s);
+        content = beforeNavigate(content);
         if (/^(https?|file):\/\//i.test(content)) {
             this.navigateToUrl(content, disposition);
             result = results.find(item => item.content === content);
