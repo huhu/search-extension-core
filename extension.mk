3531e74d974c799bcda24011fb323753d63b12e5
Folyd <lyshuhow@gmail.com>
2020-06-06 22:15:03 +0800

    Add extension.mk

---
+++ extension.mk
@@ -0,0 +1,14 @@
+chrome: core
+	@jsonnet -J core manifest.jsonnet --ext-str browser=chrome -o extension/manifest.json
+
+edge: core
+	@jsonnet -J core manifest.jsonnet --ext-str browser=edge -o extension/manifest.json
+
+firefox: core
+	@jsonnet -J core manifest.jsonnet --ext-str browser=firefox -o extension/manifest.json
+
+core: clean
+	@cp -R core/src extension/core
+
+clean:
+	@rm -rf extension/core manifest.json
