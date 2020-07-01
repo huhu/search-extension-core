ecf8a75fda0ee5d8febe696681bcc46dbe9db9af
Folyd <lyshuhow@gmail.com>
2020-07-01 22:46:12 +0800

    Remove core copy task

--- extension.mk
+++ extension.mk
@@ -1,14 +1,11 @@
-chrome: core
+chrome: clean
 	@jsonnet -J core manifest.jsonnet --ext-str browser=chrome -o extension/manifest.json
 
-edge: core
+edge: clean
 	@jsonnet -J core manifest.jsonnet --ext-str browser=edge -o extension/manifest.json
 
-firefox: core
+firefox: clean
 	@jsonnet -J core manifest.jsonnet --ext-str browser=firefox -o extension/manifest.json
-
-core: clean
-	@cp -R core/src extension/core
 
 clean:
 	@rm -rf extension/core manifest.json
