84c23666b57d44e814c8b357aa4df83cae7b9142
Folyd <lyshuhow@gmail.com>
2020-07-01 22:52:04 +0800

    Fix clean task remove files issue

--- extension.mk
+++ extension.mk
@@ -8,4 +8,4 @@ firefox: clean
 	@jsonnet -J core manifest.jsonnet --ext-str browser=firefox -o extension/manifest.json
 
 clean:
-	@rm -rf extension/core manifest.json
+	@rm -rf extension/manifest.json
