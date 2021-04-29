1ab5e656922494fdb987e547318d3c34084ae3b2
Folyd <lyshuhow@gmail.com>
2021-04-29 22:33:55 +0800

    Improve pack task

--- extension.mk
+++ extension.mk
@@ -1,3 +1,6 @@
+# https://stackoverflow.com/a/47008498/2220110
+args = `arg="$(filter-out $@,$(MAKECMDGOALS))" && echo $${arg:-${1}}`
+
 chrome: clean
 	@jsonnet -J core manifest.jsonnet --ext-str browser=chrome -o extension/manifest.json
 
@@ -7,8 +10,19 @@ edge: clean
 firefox: clean
 	@jsonnet -J core manifest.jsonnet --ext-str browser=firefox -o extension/manifest.json
 
+# Usage: make pack [browser] to build the target package for corresponding browser.
+# Example:`make pack chrome`, `make pack firefox`, `make pack edge`.
 pack:
-	web-ext build -s extension -n $(notdir $(shell pwd))-{version}.zip -o
+	@make $(call args,chrome)
+	@web-ext build -s extension -n $(call args,chrome)-$(notdir $(shell pwd))-{version}.zip -o
 
 clean:
 	@rm -rf extension/manifest.json
+
+test:
+	@echo $(call args,defaultstring)
+
+# This allows us to accept extra arguments (by doing nothing
+# when we get a job that doesn't match, rather than throwing an error).
+%:
+	@:
