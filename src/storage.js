d968fd77633d03447e52866f06ecc50b7cabfc2c
Folyd <lyshuhow@gmail.com>
2022-05-22 13:30:30 +0800

    Add migrate local storage function

--- src/storage.js
+++ src/storage.js
@@ -1,9 +1,30 @@
 // Mimic localStorage API with chrome.storage
 const storage = {
-    getAllItems: () => chrome.storage.local.get(null),
-    getItem: async key => (await chrome.storage.local.get(key))[key],
-    setItem: (key, val) => chrome.storage.local.set({
-        [key]: val
+    getAllItems: () => storageGet(null),
+    getItem: async key => (await storageGet(key))[key],
+    setItem: (key, val) => new Promise(resolve => {
+        chrome.storage.local.set({
+            [key]: val
+        }, resolve)
     }),
-    removeItems: keys => chrome.storage.local.remove(keys),
+    removeItems: keys => new Promise(resolve => {
+        chrome.storage.local.remove(keys);
+        resolve()
+    }),
 };
+
+function storageGet(key) {
+    return new Promise(resolve => {
+        chrome.storage.local.get(key, (result) => {
+            resolve(result[key]);
+        });
+    });
+}
+
+// A helper function to migrate localStorage to chrome.storage API.
+async function migrateLocalStorage(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+        await storage.setItem(key, JSON.parse(value));
+    }
+}
