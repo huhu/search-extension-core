ae9dd97e743a5869d99e060ebd57896ff0c46d59
Folyd <lyshuhow@gmail.com>
2022-08-13 16:49:15 +0800

    Change legacy codes into ES5 style

--- src/compat.js
+++ src/compat.js
@@ -1,53 +1,56 @@
-function Compat() {
-    // Firefox doesn't support tags in search suggestion.
-    this.tagged = this.browserType() !== "firefox" ?
-        (tag, str) => `<${tag}>${str}</${tag}>` :
-        (_, str) => str;
-    this.match = (str) => this.tagged("match", str);
-    this.dim = (str) => this.tagged("dim", str);
-}
+class Compat {
+    constructor() {
+        // Firefox doesn't support tags in search suggestion.
+        this.tagged = this.browserType() !== "firefox" ?
+            (tag, str) => `<${tag}>${str}</${tag}>` :
+            (_, str) => str;
+        this.match = (str) => this.tagged("match", str);
+        this.dim = (str) => this.tagged("dim", str);
+    }
 
-Compat.prototype.browserType = function () {
-    let userAgent = navigator.userAgent.toLowerCase();
-    if (userAgent.indexOf("firefox") !== -1) return "firefox";
-    if (userAgent.indexOf("edg") !== -1) return "edge";
-    if (userAgent.indexOf("chrome") !== -1) return "chrome";
-    return "unknown";
-};
+    browserType() {
+        let userAgent = navigator.userAgent.toLowerCase();
+        if (userAgent.indexOf("firefox") !== -1)
+            return "firefox";
+        if (userAgent.indexOf("edg") !== -1)
+            return "edge";
+        if (userAgent.indexOf("chrome") !== -1)
+            return "chrome";
+        return "unknown";
+    }
 
-Compat.prototype.omniboxPageSize = function () {
-    return {"firefox": 6, "edge": 7, "chrome": 8, "unknown": 6}[this.browserType()];
-};
+    omniboxPageSize() {
+        return { "firefox": 6, "edge": 7, "chrome": 8, "unknown": 6 }[this.browserType()];
+    }
 
-// Escape the five predefined entities to display them as text.
-Compat.prototype.escape = function (str) {
-    str = str || "";
-    return this.browserType() !== "firefox" ? str
+    // Escape the five predefined entities to display them as text.
+    escape(str) {
+        str = str || "";
+        return this.browserType() !== "firefox" ? str
             .replace(/&/g, "&amp;")
             .replace(/</g, "&lt;")
             .replace(/>/g, "&gt;")
             .replace(/"/g, "&quot;")
             .replace(/'/g, "&#039;")
-        : str;
-};
-
-Compat.prototype.normalizeDate = function (date) {
-    let month = '' + (date.getMonth() + 1),
-        day = '' + date.getDate(),
-        year = date.getFullYear();
-    return [year, month.padStart(2, "0"), day.padStart(2, "0")].join('-');
-};
-
-Compat.prototype.capitalize = function (value) {
-    if (value) {
-        return value.charAt(0).toUpperCase() + value.slice(1);
+            : str;
     }
-    return "";
-}
 
-Compat.prototype.eliminateTags = function (value) {
-    if (value) {
-        return value.replace(/<\/?(match|dim|code|em|strong)>/g, "");
+    normalizeDate(date) {
+        let month = '' + (date.getMonth() + 1), day = '' + date.getDate(), year = date.getFullYear();
+        return [year, month.padStart(2, "0"), day.padStart(2, "0")].join('-');
     }
-    return "";
+
+    capitalize(value) {
+        if (value) {
+            return value.charAt(0).toUpperCase() + value.slice(1);
+        }
+        return "";
+    }
+
+    eliminateTags(value) {
+        if (value) {
+            return value.replace(/<\/?(match|dim|code|em|strong)>/g, "");
+        }
+        return "";
+    }
 }
--- src/toast.js
+++ src/toast.js
@@ -1,34 +1,36 @@
-function Toast(selector) {
-    this.element = document.querySelector(selector);
-    this.dismissTimeoutId = null;
-    this.element.addEventListener("mouseover", () => {
-        if (this.dismissTimeoutId) {
-            clearTimeout(this.dismissTimeoutId);
-        }
-    });
-    this.element.addEventListener("mouseleave", () => {
-        this.dismiss();
-    });
+class Toast {
+    constructor(selector) {
+        this.element = document.querySelector(selector);
+        this.dismissTimeoutId = null;
+        this.element.addEventListener("mouseover", () => {
+            if (this.dismissTimeoutId) {
+                clearTimeout(this.dismissTimeoutId);
+            }
+        });
+        this.element.addEventListener("mouseleave", () => {
+            this.dismiss();
+        });
+    }
+
+    info(message) {
+        this.element.style.display = "block";
+        this.element.style.background = "#fbeca0dd";
+        this.element.textContent = message;
+    }
+
+    success(message) {
+        this.element.style.display = "block";
+        this.element.style.background = "#357911dd";
+        this.element.textContent = message;
+    }
+
+    error(message) {
+        this.element.style.display = "block";
+        this.element.style.background = "#ff0000dd";
+        this.element.textContent = message;
+    }
+
+    dismiss(delay = 2000) {
+        this.dismissTimeoutId = setTimeout(() => this.element.style.display = "none", delay);
+    }
 }
-
-Toast.prototype.info = function(message) {
-    this.element.style.display = "block";
-    this.element.style.background = "#fbeca0dd";
-    this.element.textContent = message;
-};
-
-Toast.prototype.success = function(message) {
-    this.element.style.display = "block";
-    this.element.style.background = "#357911dd";
-    this.element.textContent = message;
-};
-
-Toast.prototype.error = function(message) {
-    this.element.style.display = "block";
-    this.element.style.background = "#ff0000dd";
-    this.element.textContent = message;
-};
-
-Toast.prototype.dismiss = function(delay = 2000) {
-    this.dismissTimeoutId = setTimeout(() => this.element.style.display = "none", delay);
-};
