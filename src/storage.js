0c40a3955c2dd6bf3591bffc4fb2c0b334e093a0
Folyd <lyshuhow@gmail.com>
2024-06-01 11:36:33 -0700

    Support localStorage

--- src/storage.js
+++ src/storage.js
@@ -1,43 +1,65 @@
-// Mimic localStorage API with chrome.storage.
-// See also: https://developer.chrome.com/docs/extensions/reference/storage/
-const storage = {
-    getAllItems: () => new Promise(resolve => {
-        chrome.storage.local.get(null, resolve);
-    }),
-    // Gets one or more items from storage.
-    getItem: key => new Promise(resolve => {
-        chrome.storage.local.get(key, (result) => {
-            if (result) {
-                resolve(result[key]);
-            } else {
-                resolve(null);
-            }
-        });
-    }),
-    // Set signle key-value pair item.
-    setItem: (key, val) => new Promise(resolve => {
-        chrome.storage.local.set({
-            [key]: val
-        }, resolve)
-    }),
-    // Removes one or more items from storage.
-    removeItem: key => new Promise(resolve => {
-        chrome.storage.local.remove(key, resolve);
-    }),
-};
+let storage = null;
 
-// A helper function to migrate localStorage to chrome.storage API.
-async function migrateLocalStorage(key) {
-    let value = localStorage.getItem(key);
-    if (value) {
-        try {
-            // Some plain text isn't JSON parsable.
-            value = JSON.parse(value);
-        } catch (e) {
-            console.log(`can't parse ${value} as JSON: ${e}`);
-        }
-        await storage.setItem(key, value);
-    }
+if (chrome && chrome.storage) {
+    // Mimic localStorage API with chrome.storage.
+    // See also: https://developer.chrome.com/docs/extensions/reference/storage/
+    storage = {
+        getAllItems: () => new Promise(resolve => {
+            chrome.storage.local.get(null, resolve);
+        }),
+        // Gets one or more items from storage.
+        getItem: key => new Promise(resolve => {
+            chrome.storage.local.get(key, (result) => {
+                if (result) {
+                    resolve(result[key]);
+                } else {
+                    resolve(null);
+                }
+            });
+        }),
+        // Set signle key-value pair item.
+        setItem: (key, val) => new Promise(resolve => {
+            chrome.storage.local.set({
+                [key]: val
+            }, resolve)
+        }),
+        // Removes one or more items from storage.
+        removeItem: key => new Promise(resolve => {
+            chrome.storage.local.remove(key, resolve);
+        }),
+    };
+} else {
+    storage = {
+        getAllItems: () => new Promise(resolve => {
+            // chrome.storage.local.get(null, resolve);
+        }),
+        // Gets one or more items from storage.
+        getItem: key => new Promise(resolve => {
+            let val = localStorage.getItem(key);
+            if (val) {
+                try {
+                    val = JSON.parse(val);
+                } catch (e) {
+                    // Ignore.
+                }
+            }
+            resolve(val);
+        }),
+        // Set signle key-value pair item.
+        setItem: (key, val) => new Promise(resolve => {
+            if (val instanceof Object) {
+                val = JSON.stringify(val);
+            }
+            localStorage.setItem(key, val);
+            resolve();
+        }),
+        // Removes one or more items from storage.
+        removeItem: key => new Promise(resolve => {
+            // chrome.storage.local.remove(key, resolve);
+            localStorage.removeItem(key);
+            resolve();
+        }),
+    };
 }
 
 export default storage;
