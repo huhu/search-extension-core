d73f3db6ece996d07f376ac770f0c18abaeb6b51
Folyd <lyshuhow@gmail.com>
2024-06-19 19:43:04 +0000

    Reset search keyword after navigated

--- src/omnibox.js
+++ src/omnibox.js
@@ -224,6 +224,8 @@ export default class Omnibox {
 
             if (this.extensionMode) {
                 this.setDefaultSuggestion(this.defaultSuggestionDescription);
+            } else {
+                this.render.resetSearchKeyword();
             }
         });
     }
