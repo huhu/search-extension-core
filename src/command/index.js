5d69cd5a45a3b5d7642e73962256627f5be4bfab
Folyd <lyshuhow@gmail.com>
2024-06-23 19:07:55 -0700

    Simplify exports

---
+++ src/command/index.js
@@ -0,0 +1,7 @@
+import Command from "./base.js";
+import HistoryCommand from "./history.js";
+import CommandManager from "./manager.js";
+import OpenCommand from "./open.js";
+import SimpleCommand from "./simple.js";
+
+export { Command, HistoryCommand, CommandManager, OpenCommand, SimpleCommand };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import Compat from "./compat.js";
 import Omnibox from "./omnibox.js";
 import QueryEvent from "./query-event.js";
 import Render from "./render.js";
+import storage from "./storage.js";
+
+export * from "./command/index.js";
 
 export {
     Omnibox,
     QueryEvent,
     Render,
     Compat,
+    storage,
 }
